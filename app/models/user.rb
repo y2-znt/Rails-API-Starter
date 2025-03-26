5b166a5bd755045886dd3733ce3b475ca6c0b66b
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-03-26 21:48:41 +0100

    feat: add schema annotations to models and fixtures
    This commit adds schema information annotations to the `Group` and `User` models, as well as their corresponding test files and fixtures. The annotations provide a clear overview of the database structure for each model, enhancing documentation and readability.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  password   :string           not null
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
