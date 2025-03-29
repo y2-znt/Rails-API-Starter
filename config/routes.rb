023d8827951b352276b0cdffe42ad4773b99735f
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-03-29 18:07:55 +0100

    chore: remove GroupsController, Group model, related tests, and database migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create, :show, :update, :destroy]
-      resources :groups, only: [:index, :create, :show, :update, :destroy]
     end
   end
 end
