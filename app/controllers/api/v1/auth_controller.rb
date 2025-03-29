9c19d732946190192a6fc0e58ddbdc5ae498d8b5
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-03-29 22:17:28 +0100

    feat: implement AuthController for user registration and login
    This commit introduces the AuthController, which provides endpoints for user registration and login. The registration action creates a new user and returns success or error messages, while the login action authenticates users and generates a JWT token upon successful login.

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::AuthController < ApplicationController
+  skip_before_action :authenticate_request, only: [:register, :login]
+
+  def register
+    user = User.create(user_params)
+    if user.save
+      render json: {message: "User created successfully"}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: user.id)
+      render json: {token: token}, status: :ok
+    else
+      render json: {error: "Invalid email or password"}, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+end
