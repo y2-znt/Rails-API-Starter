be87ad136734426d7986f3f02962a695ce7cd731
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-03-29 22:17:45 +0100

    feat: add authentication routes for user registration, login, and profile access

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create, :show, :update, :destroy]
+      post "auth/register", to: "auth#register"
+      post "auth/login", to: "auth#login"
+      get "auth/profile", to: "auth#profile"
     end
   end
 end
