504672bfd3c3fbeac98fbdf8431a332adf424e0c
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-03-30 13:32:28 +0200

    feat: add role management to users with enum type

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_29_165727) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_30_102027) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "user_role", ["admin", "user"]
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.enum "role", default: "user", null: false, enum_type: "user_role"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
