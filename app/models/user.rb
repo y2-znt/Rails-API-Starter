0c60b0689b770876a524abceaf9ec4325ea70fb0
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-04-01 09:07:43 +0200

    fix: enhance email validation in User model to include format check

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   enum :role, {user: "user", admin: "admin"}
 
   validates :username, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates :password, presence: true, length: {minimum: 8}, if: :validate_password?
 
   private
